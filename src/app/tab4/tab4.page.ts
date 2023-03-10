8b1484cd5fec580bac4475748646e4433c9aba6e
Rashi Chandola <rashi_chandola@Rashis-MacBook-Air.local>
2023-03-10 23:17:25 +0530

    basic functionality

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { CalendarMode, Step } from 'ionic2-calendar';
+import { Component, ViewChild } from '@angular/core';
+import { CalendarMode, QueryMode, Step } from 'ionic2-calendar';
+import { CalendarComponent } from 'ionic2-calendar';
 import data from "src/assets/journey.json";
 @Component({
   selector: 'app-tab4',
@@ -7,52 +8,238 @@ import data from "src/assets/journey.json";
   styleUrls: ['tab4.page.scss']
 })
 export class Tab4Page {
-  eventSource;
+  @ViewChild(CalendarComponent) myCalendar!: CalendarComponent;
+
+  constructor() {
+    this.isToday = false;
+  }
+
+  eventSource: any = [];
+  viewTitle: any;
+  isToday: boolean;
+
   calendar = {
     mode: 'month' as CalendarMode,
+    queryMode: 'local' as QueryMode,
     step: 30 as Step,
     currentDate: new Date(),
     dateFormatter: {
-      formatMonthViewDay: function(date:Date) {
-          return date.getDate().toString();
+      formatMonthViewDay: function (date: Date) {
+        return date.getDate().toString();
       },
-      formatMonthViewDayHeader: function(date:Date) {
-          return 'testMDH';
+      formatMonthViewDayHeader: function (date: Date) {
+        return 'MonMH';
       },
-      formatMonthViewTitle: function(date:Date) {
-          return 'testMT';
+      formatMonthViewTitle: function (date: Date) {
+        return 'testMT';
       },
-      formatWeekViewDayHeader: function(date:Date) {
-          return 'testWDH';
+      formatWeekViewDayHeader: function (date: Date) {
+        return 'MonWH';
       },
-      formatWeekViewTitle: function(date:Date) {
-          return 'testWT';
+      formatWeekViewTitle: function (date: Date) {
+        return 'testWT';
       },
-      formatWeekViewHourColumn: function(date:Date) {
-          return 'testWH';
+      formatWeekViewHourColumn: function (date: Date) {
+        return 'testWH';
       },
-      formatDayViewHourColumn: function(date:Date) {
-          return 'testDH';
+      formatDayViewHourColumn: function (date: Date) {
+        return 'testDH';
       },
-      formatDayViewTitle: function(date:Date) {
-          return 'testDT';
-      }
+      formatDayViewTitle: function (date: Date) {
+        return 'testDT';
+      },
+    },
+    formatDay: "'Day' dd",
+    formatDayHeader: "'Day' EEE",
+    formatDayTitle: "'Day' MMMM dd, yyyy",
+    formatWeekTitle: "'Week' w",
+    formatWeekViewDayHeader: "'Day' EEE d",
+    formatHourColumn: "'hour' ha",
+    showEventDetail: false,
+    startingDayMonth: 2,
+    startingDayWeek: 2,
+    allDayLabel: 'testallday',
+    noEventsLabel: 'None',
+    timeInterval: 15,
+    autoSelect: false,
+    locale: 'zh-CN',
+    dir: 'rtl',
+    scrollToHour: 3,
+    preserveScrollPosition: true,
+    lockSwipeToPrev: true,
+    lockSwipeToNext: true,
+    lockSwipes: true,
+    startHour: 9,
+    endHour: 16,
+    sliderOptions: {
+      spaceBetween: 10,
+    },
+  };
+
+  loadEvents() {
+    this.eventSource;
   }
+
+  loadDynamicEvents() {
+    let startTime = new Date('2023-01-20T03:00:40');
+    let endTime = new Date('2023-01-22T05:39:22');
+
+    this.eventSource.push({
+      title: 'test',
+      startTime: startTime,
+      endTime: endTime,
+      allDay: false,
+    });
+    this.myCalendar.loadEvents();
+  }
+
+  onViewTitleChanged(title: string) {
+    this.viewTitle = title;
+    console.log(
+      'view title changed: ' + title + ', this.viewTitle: ' + this.viewTitle
+    );
+  }
+
+  onEventSelected(event: any) {
+    console.log(
+      'Event selected:' +
+        event.startTime +
+        '-' +
+        event.endTime +
+        ',' +
+        event.title
+    );
+  }
+
+  changeMode(mode: any) {
+    this.calendar.mode = mode;
+  }
+
+  today() {
+    this.calendar.currentDate = new Date();
+  }
+
+  onTimeSelected
+  (ev: { selectedTime: Date, events: any[], disabled: boolean }) 
+  {
+    this.addEvent(ev.selectedTime)
 };
 
-  constructor() {
-    
-  }
 
-  onCurrentChanged = (ev: Date) => {
+  onCurrentDateChanged(ev: Date) {
+    // this.addEvent(ev);
+    var today = new Date();
+    today.setHours(0, 0, 0, 0);
+    ev.setHours(0, 0, 0, 0);
+    this.isToday = today.getTime() === ev.getTime();
     console.log('Currently viewed date: ' + ev);
+  }
+
+  addEvent(ev: Date) {
+    var startTime = new Date(
+      Date.UTC(
+        ev.getUTCFullYear(),
+        ev.getUTCMonth(),
+        ev.getUTCDate(),
+        ev.getUTCDate(),
+        ev.getHours(),
+        ev.getMinutes()
+      )
+    );
+    var endTime = new Date(
+      Date.UTC(
+        startTime.getUTCFullYear(),
+        startTime.getUTCMonth(),
+        startTime.getUTCDate(),
+        startTime.getHours(),
+        startTime.getMinutes(),
+        startTime.getSeconds() + 30
+      )
+    );
+    this.eventSource.push({
+      title: 'Appointment',
+      startTime,
+      endTime,
+      allDay: false,
+    });
+  }
+
+  createRandomEvents() {
+    var events = [{}];
+    for (var i = 0; i < 100; i += 1) {
+      var date = new Date();
+      var eventType = Math.floor(Math.random() * 2);
+      var startDay = Math.floor(Math.random() * 90) - 45;
+      var endDay = Math.floor(Math.random() * 2) + startDay;
+      var startTime;
+      var endTime;
+      if (eventType === 0) {
+        startTime = new Date(
+          Date.UTC(
+            date.getUTCFullYear(),
+            date.getUTCMonth(),
+            date.getUTCDate() + startDay
+          )
+        );
+        if (endDay === startDay) {
+          endDay += 1;
+        }
+        endTime = new Date(
+          Date.UTC(
+            date.getUTCFullYear(),
+            date.getUTCMonth(),
+            date.getUTCDate() + endDay
+          )
+        );
+        events.push({
+          title: 'All Day - ' + i,
+          startTime: startTime,
+          endTime: endTime,
+          allDay: true,
+        });
+      } else {
+        var startMinute = Math.floor(Math.random() * 24 * 60);
+        var endMinute = Math.floor(Math.random() * 180) + startMinute;
+        startTime = new Date(
+          date.getFullYear(),
+          date.getMonth(),
+          date.getDate() + startDay,
+          0,
+          date.getMinutes() + startMinute
+        );
+        endTime = new Date(
+          date.getFullYear(),
+          date.getMonth(),
+          date.getDate() + endDay,
+          0,
+          date.getMinutes() + endMinute
+        );
+        events.push({
+          title: 'Event - ' + i,
+          startTime: startTime,
+          endTime: endTime,
+          allDay: false,
+        });
+      }
+    }
+    return events;
+  }
+
+  // onRangeChanged(ev: any) {
+  //   console.log(
+  //     'range changed: startTime: ' + ev.startTime + ', endTime: ' + ev.endTime
+  //   );
+  //   this.eventSource = this.createRandomEvents();
+  // }
+
+  onDayHeaderSelected = (ev: { selectedTime: Date, events: any[], disabled: boolean }) => {
+    console.log('Selected day: ' + ev.selectedTime + ', hasEvents: ' + (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
 };
 
-onEventSelected = (event) => {
-  console.log(event.title);
-};
-onTimeSelected = (ev: { selectedTime: Date, events: any[], disabled: boolean }) => {
-  console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' + (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
-};
+  markDisabled = (date: Date) => {
+    var current = new Date();
+    current.setHours(0, 0, 0);
+    return date < current;
+  };
 
 }
